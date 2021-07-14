50d86c2afc4af02db282ad8baecb8ac831a013de
anonim <mertcaliskanlinux@gmail.com>
2021-07-14 22:02:04 +0300

    DjangoOrderTracking

---
+++ dashboard/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Product,Order
+from django.contrib.auth.models import Group
+
+
+admin.site.site_header = 'MÇ Inventroy Dashboard'
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','quantity','category')
+    list_filter = ['category']
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Order)
