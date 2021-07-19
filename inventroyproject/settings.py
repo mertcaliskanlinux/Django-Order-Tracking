0803e6771e57f259bd00aab6c8b085602b8d0c17
mertlinux <77293848+mertcaliskanlnx@users.noreply.github.com>
2021-07-19 05:23:10 +0300

    Update settings.py

--- inventroyproject/settings.py
+++ inventroyproject/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'testkony18@gmail.com'
-EMAIL_HOST_PASSWORD = '1234567890m*m'
+EMAIL_HOST_USER = 'YOUR EMAİL ' #mertcaliskanlinux@gmail.com
+EMAIL_HOST_PASSWORD = 'YOUR EMAİL PASSWORD '
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
